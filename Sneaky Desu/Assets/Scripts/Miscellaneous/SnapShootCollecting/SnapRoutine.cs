305398f62479d3cc5b195fcf0453307dae5abdd3
Tyrease Teer <46007223+HarashiGouse2019@users.noreply.github.com>
2019-04-24 15:20:35 -0600

    Update

---
+++ Sneaky Desu/Assets/Scripts/Miscellaneous/SnapShootCollecting/SnapRoutine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapRoutine : MonoBehaviour
+{
+    int i = 0;
+    float time = 10f;
+    float resetTime;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        resetTime = time;
+    }
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time < 1)
+        {
+            ScreenCapture.CaptureScreenshot(@"C:\Users\Tokusunei\Pictures\Juntohaizen Snapshoots\Juntohaizen" + i.ToString("D3") + ".png", 4);
+            ++i;
+            time = resetTime;
+        }
+    }
+}
